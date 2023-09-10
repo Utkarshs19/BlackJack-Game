d2e87acd7182dfeb823480bdc47b8340c083aed2
Utkarsh <Utkarsh.sharma19072002@gmail.com>
2023-09-10 16:15:10 +0530

    First Project

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-document.getElementById(count).innerText=5
+let count =0;
+
+let countEl=document.getElementById("count-el")
+console.log(countEl)
+
+function decrement()
+{
+    count--;
+    countEl.innerText=count
+}
+
+function increment(){
+    count++;
+    countEl.innerText=count;
+    // console.log(count)
+}
+
